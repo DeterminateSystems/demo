4e7510aec0c1198202069a0e360b97bf4afef9e4
Graham Christensen <graham@grahamc.com>
2024-09-24 13:33:12 -0400

    disable ethercalc

--- flake.nix
+++ flake.nix
@@ -36,8 +36,8 @@
                     networking.firewall.allowedTCPPorts = [ 80 ];
                     systemd.services.ethercalc.serviceConfig.AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
                     services.ethercalc = {
-                        enable = true;
-                        port = 81;
+                        enable = false;
+                        port = 80;
                     };
 
                     services.writefreely = {
